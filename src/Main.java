b64f1334fadd17d5a8395224054319ba00c552a2
VictorTimbo <manoelvictortimbo@gmail.com>
2025-01-18 15:54:31 -0300

    feature: create entities

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
+import model.entities.Department;
+import model.entities.Seller;
+
+import java.util.Date;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Department department = new Department(1, "books");
+        Seller seller = new Seller(21, "Victor", "manoelvictortimbo@gmail.com", new Date(), 3500.5, department);
+        System.out.println(seller);
     }
 }
